6901a7a9538cc4896b3907b999f90b188dee9114
crolvlee <135516362+crolvlee@users.noreply.github.com>
2024-10-13 13:46:57 +0900

    [level 2] Title: 구명보트, Time: 16.76 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,56 +1,28 @@
-# # 투 포인터
+from collections import deque
 
-# from collections import deque
-
-# def solution(people, limit):
-#     people.sort()   # [50, 50, 70, 80]
-    
-#     # 현재 보트의 개수
-#     boat = 0
-    
-#     # 투포인터 초기화
-#     a = 0
-#     b = len(people)-1
-    
-#     # 현재 보트에 탄 인원의 무게 합
-#     current = 0
-    
-#     # 보트에 태우기
-#     while a <= b:
-#         if a == b:
-#             if current + people[b] <= limit:
-#                 boat += 1
-#             else:
-#                 boat += 2
-#             b -= 1
-#         else:
-#             if current + people[b] <= limit:
-#                 current += people[b]
-#                 b -= 1
-#             elif current + people[a] <= limit:
-#                 current += people[a]
-#                 a += 1
-#             else:
-#                 current = 0
-#                 boat += 1
-        
-        
-#     answer = boat
-#     return answer
 def solution(people, limit):
-    first = 0
-    last = len(people) - 1
-    answer = 0
+    people.sort(reverse=True)
+    people = deque(people)
+    
+    # 배의 개수
+    cnt = 0
+    
+    # [40, 30, 20, 20, 10]
+    # [30, 20, 20]
+    # [20]
+    
+    while people:
+        if len(people) == 1:
+            cnt += 1
+            break
+        
+        elif len(people) >= 2:
+            if people[0] + people[-1] <= limit:
+                people.pop()    # 인덱스 -1인 원소 pop
+            people.popleft()       # 인덱스 0인 원소 pop
+            cnt += 1
 
-    people.sort(reverse = True)
-
-    while first <= last:
-
-        if people[first] + people[last] <= limit:
-            last -= 1
-
-        first += 1
-        answer += 1
-
-
+    
+    
+    answer = cnt
     return answer

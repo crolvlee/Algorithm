384e9608d41ff29c55672d7b738d21f3bfdde058
crolvlee <135516362+crolvlee@users.noreply.github.com>
2025-08-10 12:10:28 +0900

    [level 2] Title: 호텔 대실, Time: 1.90 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/README.md
+++ 프로그래머스/2/155651. 호텔 대실/README.md
@@ -1,10 +1,10 @@
 # [level 2] 호텔 대실 - 155651 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 33.10 ms
+메모리: 9.43 MB, 시간: 1.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 25일 19:38:17
+2025년 08월 10일 12:10:22
 
 ### 문제 설명
 
--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,38 +1,20 @@
 # 시작 시간은 자정을 넘어가지 않음
 # 종료 시간 + 청소시간은 자정을 넘어갈 수도 있음
 
+import heapq
+
 def solution(book_time):
-    fill_time = []
-    for bt in book_time:
-        start_hour, start_min = bt[0].split(':')
-        end_hour, end_min = bt[1].split(':')
-        
-        start = int(start_hour) * 60 + int(start_min)
-        end = int(end_hour) * 60 + int(end_min) + 10
-        fill_time.append([start, end])
+    book_time.sort()
+    now_room = []
     
-    fill_time.sort()
-    # print(fill_time)
+    for s_t, e_t in book_time:
+        start_time = int(s_t[0:2]) * 60 + int(s_t[3:5])
+        end_time = int(e_t[0:2]) * 60 + int(e_t[3:5])
+        if now_room and now_room[0] <= start_time:
+            heapq.heappop(now_room)
+            heapq.heappush(now_room, end_time + 10)
+        else:
+            heapq.heappush(now_room, end_time + 10)
     
-    timeline = []   # [[1, 1160], [2, 920]]
-    for ft in fill_time:
-        # timeline에 아무것도 없는 경우
-        if len(timeline) == 0:
-            timeline.append([1, ft[1]])
-            continue
-            
-        # timeline에 뭔가 있는 경우
-        enter = False
-        for tl in timeline:
-            if tl[1] <= ft[0]:
-                tl[1] = ft[1]
-                enter = True
-                break
-        if enter == False:
-            timeline.append([len(timeline) + 1, ft[1]])
-        
-        # print("---------")
-        # print(timeline)
-        
-    answer = len(timeline)
+    answer = len(now_room)
     return answer

17e9abafd1840ef141ae7602f2e378a26db03e8b
crolvlee <135516362+crolvlee@users.noreply.github.com>
2025-10-10 16:40:56 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 612 ms, Memory: 114036 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -1,20 +1,22 @@
+# 입력받기
 N = int(input())
-A = []
+lst = []
 
 for _ in range(N):
-    u, v = map(int, input().split())
-    A.append([u, v])
-
+    r, c = map(int, input().split())
+    lst.append([r, c])
+    
+# dp 배열
 dp = [[0] * N for _ in range(N)]
 
-# d = 간격
-for d in range(1, N):
+# d = start와 end 차이
+for d in range(1, N+1):
     for start in range(0, N-d):
         end = start + d
         dp[start][end] = 1e9
         
         for mid in range(start, end):
-            cost = dp[start][mid] + dp[mid + 1][end] + (A[start][0] * A[mid][1] * A[end][1])
+            cost = dp[start][mid] + dp[mid+1][end] + (lst[start][0] * lst[mid][1] * lst[end][1])
             dp[start][end] = min(dp[start][end], cost)
             
 print(dp[0][N-1])
